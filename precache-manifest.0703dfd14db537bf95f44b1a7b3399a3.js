3472612db22e1f2f6f1586af6ff9f4571d8845f4
s777610 <s777610@gmail.com>
2019-04-21 22:53:02 -0700

    Updates

---
+++ precache-manifest.0703dfd14db537bf95f44b1a7b3399a3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ff162a1c66bc60bf1e2da00851eebf",
+    "url": "/sing-color-frontend-demo/static/media/pic-3.a6ff162a.jpg"
+  },
+  {
+    "revision": "61dbdb3809c5cd98b168",
+    "url": "/sing-color-frontend-demo/static/css/main.c1f766f6.chunk.css"
+  },
+  {
+    "revision": "de2c25633421f71ccb03",
+    "url": "/sing-color-frontend-demo/static/js/runtime~main.7b6c938b.js"
+  },
+  {
+    "revision": "6ebb8a55f6df25214432",
+    "url": "/sing-color-frontend-demo/static/js/2.0739cd8d.chunk.js"
+  },
+  {
+    "revision": "bf6f24c68daf9c26284a5a1894e1c594",
+    "url": "/sing-color-frontend-demo/static/media/sing_logo.bf6f24c6.png"
+  },
+  {
+    "revision": "84d623b3b2e95bf622c567afd6d59235",
+    "url": "/sing-color-frontend-demo/static/media/pic-1.84d623b3.jpg"
+  },
+  {
+    "revision": "b095caf608cbad579cbece149ea37471",
+    "url": "/sing-color-frontend-demo/static/media/pic-2.b095caf6.jpg"
+  },
+  {
+    "revision": "61dbdb3809c5cd98b168",
+    "url": "/sing-color-frontend-demo/static/js/main.24a45b2e.chunk.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/sing-color-frontend-demo/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/sing-color-frontend-demo/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "2df42de0d4476a62a45dc3b255d90290",
+    "url": "/sing-color-frontend-demo/static/media/booking.2df42de0.jpg"
+  },
+  {
+    "revision": "5c4c1d9b5c146b952300d0a0454cf54b",
+    "url": "/sing-color-frontend-demo/static/media/new_background.5c4c1d9b.jpg"
+  },
+  {
+    "revision": "3acfc980c57d0f4493a9d2fcdf51a475",
+    "url": "/sing-color-frontend-demo/static/media/man.3acfc980.jpg"
+  },
+  {
+    "revision": "9b41085cc8f25006d964cb734804e012",
+    "url": "/sing-color-frontend-demo/index.html"
+  }
+];
